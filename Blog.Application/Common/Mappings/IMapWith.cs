33ae610361de37de54b30630095d95e2d7a33913
yurii <urakarabin123@gmail.com>
2022-08-20 19:19:44 +0300

    add automapper

---
+++ Blog.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace Blog.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
