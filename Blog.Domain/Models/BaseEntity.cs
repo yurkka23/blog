aef1e02eca472865d786b6c5f23dbe300212de89
yurii <urakarabin123@gmail.com>
2022-08-20 16:37:52 +0300

    Add core , create models and enums for models

---
+++ Blog.Domain/Models/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Domain.Models
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+}
---
+++ Blog.Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+
+namespace Blog.Domain.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutMe { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
